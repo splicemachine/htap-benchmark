9783ea5074a3a2a71379417ce6f385f5ce2c6be3
RodB <rodbutters@gmail.com>
2018-09-10 15:27:07 -0700

    corrected path in load script
    Load script was pointing to cloud accessible s3 bucket. Changed to public bucket.

--- quickstart/load-htap1000.sql
+++ quickstart/load-htap1000.sql
@@ -214,13 +214,13 @@ GROUP BY supplier_no;
 
 -- something quick to start to ensure everything is working
 
-call SYSCS_UTIL.IMPORT_DATA (current schema,'WAREHOUSE','W_ID,W_YTD,W_TAX,W_NAME,W_STREET_1,W_STREET_2,W_CITY,W_STATE,W_ZIP,W_NATIONKEY','s3a://htap-b/flat/HTAP/htap-1000/warehouse.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://htap-b/flat/HTAP/data-errors', true, null);
+call SYSCS_UTIL.IMPORT_DATA (current schema,'WAREHOUSE','W_ID,W_YTD,W_TAX,W_NAME,W_STREET_1,W_STREET_2,W_CITY,W_STATE,W_ZIP,W_NATIONKEY','s3a://splice-benchmark-data/flat/HTAP/htap-1000/warehouse.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://splice-benchmark-data/flat/HTAP/data-errors', true, null);
 
 -- next processing STOCK - requires two step process to add S_SUPPKEY to data file
 
 call SYSCS_UTIL.IMPORT_DATA (current schema,'MODSTOCK',
    'S_W_ID,S_I_ID,S_QUANTITY,S_YTD,S_ORDER_CNT,S_REMOTE_CNT,S_DATA,S_DIST_01,S_DIST_02,S_DIST_03,S_DIST_04,S_DIST_05,S_DIST_06,S_DIST_07,S_DIST_08,S_DIST_09,S_DIST_10',
-   's3a://htap-b/flat/HTAP/htap-1000/stock.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://htap-b/flat/HTAP/data-errors', true, null);
+   's3a://splice-benchmark-data/flat/HTAP/htap-1000/stock.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://splice-benchmark-data/flat/HTAP/data-errors', true, null);
 
 INSERT INTO STOCK SELECT 
 S_W_ID,S_I_ID,S_QUANTITY,S_YTD,S_ORDER_CNT,S_REMOTE_CNT,S_DATA,S_DIST_01,S_DIST_02,S_DIST_03,S_DIST_04,S_DIST_05,S_DIST_06,S_DIST_07,S_DIST_08,S_DIST_09,S_DIST_10,
@@ -230,29 +230,29 @@ DROP TABLE MODSTOCK;
 
 -- process remainder of data files
 
-call SYSCS_UTIL.IMPORT_DATA (current schema,'CUSTOMER','C_W_ID,C_D_ID,C_ID,C_DISCOUNT,C_CREDIT,C_LAST,C_FIRST,C_CREDIT_LIM,C_BALANCE,C_YTD_PAYMENT,C_PAYMENT_CNT,C_DELIVERY_CNT,C_STREET_1,C_STREET_2,C_CITY,C_STATE,C_ZIP,C_NATIONKEY,C_PHONE,C_SINCE,C_MIDDLE,C_DATA','s3a://htap-b/flat/HTAP/htap-1000/customer.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://htap-b/flat/HTAP/data-errors', true, null);
+call SYSCS_UTIL.IMPORT_DATA (current schema,'CUSTOMER','C_W_ID,C_D_ID,C_ID,C_DISCOUNT,C_CREDIT,C_LAST,C_FIRST,C_CREDIT_LIM,C_BALANCE,C_YTD_PAYMENT,C_PAYMENT_CNT,C_DELIVERY_CNT,C_STREET_1,C_STREET_2,C_CITY,C_STATE,C_ZIP,C_NATIONKEY,C_PHONE,C_SINCE,C_MIDDLE,C_DATA','s3a://splice-benchmark-data/flat/HTAP/htap-1000/customer.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://splice-benchmark-data/flat/HTAP/data-errors', true, null);
 
 call SYSCS_UTIL.IMPORT_DATA (current schema,'DISTRICT',
    'D_W_ID,D_ID,D_YTD,D_TAX,D_NEXT_O_ID,D_NAME,D_STREET_1,D_STREET_2,D_CITY,D_STATE,D_ZIP,D_NATIONKEY',
-   's3a://htap-b/flat/HTAP/htap-1000/district.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://htap-b/flat/HTAP/data-errors', true, null);
+   's3a://splice-benchmark-data/flat/HTAP/htap-1000/district.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://splice-benchmark-data/flat/HTAP/data-errors', true, null);
 
-call SYSCS_UTIL.IMPORT_DATA (current schema,'HISTORY','H_C_ID,H_C_D_ID,H_C_W_ID,H_D_ID,H_W_ID,H_DATE,H_AMOUNT,H_DATA','s3a://htap-b/flat/HTAP/htap-1000/history.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://htap-b/flat/HTAP/data-errors', true, null);
+call SYSCS_UTIL.IMPORT_DATA (current schema,'HISTORY','H_C_ID,H_C_D_ID,H_C_W_ID,H_D_ID,H_W_ID,H_DATE,H_AMOUNT,H_DATA','s3a://splice-benchmark-data/flat/HTAP/htap-1000/history.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://splice-benchmark-data/flat/HTAP/data-errors', true, null);
 
-call SYSCS_UTIL.IMPORT_DATA (current schema,'ITEM','I_ID,I_NAME,I_PRICE,I_DATA,I_IM_ID','s3a://htap-b/flat/HTAP/htap-1000/item.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://htap-b/flat/HTAP/data-errors', true, null);
+call SYSCS_UTIL.IMPORT_DATA (current schema,'ITEM','I_ID,I_NAME,I_PRICE,I_DATA,I_IM_ID','s3a://splice-benchmark-data/flat/HTAP/htap-1000/item.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://splice-benchmark-data/flat/HTAP/data-errors', true, null);
 
-call SYSCS_UTIL.IMPORT_DATA (current schema,'NEW_ORDER','NO_W_ID,NO_D_ID,NO_O_ID','s3a://htap-b/flat/HTAP/htap-1000/new_order.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://htap-b/flat/HTAP/data-errors', true, null);
+call SYSCS_UTIL.IMPORT_DATA (current schema,'NEW_ORDER','NO_W_ID,NO_D_ID,NO_O_ID','s3a://splice-benchmark-data/flat/HTAP/htap-1000/new_order.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://splice-benchmark-data/flat/HTAP/data-errors', true, null);
 
-call SYSCS_UTIL.IMPORT_DATA (current schema,'OORDER','O_W_ID,O_D_ID,O_ID,O_C_ID,O_CARRIER_ID,O_OL_CNT,O_ALL_LOCAL,O_ENTRY_D','s3a://htap-b/flat/HTAP/htap-1000/oorder.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://htap-b/flat/HTAP/data-errors', true, null);
+call SYSCS_UTIL.IMPORT_DATA (current schema,'OORDER','O_W_ID,O_D_ID,O_ID,O_C_ID,O_CARRIER_ID,O_OL_CNT,O_ALL_LOCAL,O_ENTRY_D','s3a://splice-benchmark-data/flat/HTAP/htap-1000/oorder.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://splice-benchmark-data/flat/HTAP/data-errors', true, null);
 
 call SYSCS_UTIL.IMPORT_DATA (current schema,'ORDER_LINE',
    'OL_W_ID,OL_D_ID,OL_O_ID,OL_NUMBER,OL_I_ID,OL_DELIVERY_D,OL_AMOUNT,OL_SUPPLY_W_ID,OL_QUANTITY,OL_DIST_INFO',
-   's3a://htap-b/flat/HTAP/htap-1000/order_line.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://htap-b/flat/HTAP/data-errors', true, null);
+   's3a://splice-benchmark-data/flat/HTAP/htap-1000/order_line.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://splice-benchmark-data/flat/HTAP/data-errors', true, null);
 
-call SYSCS_UTIL.IMPORT_DATA (current schema,'REGION','R_REGIONKEY,R_NAME,R_COMMENT','s3a://htap-b/flat/HTAP/htap-1000/region.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://htap-b/flat/HTAP/data-errors', true, null);
+call SYSCS_UTIL.IMPORT_DATA (current schema,'REGION','R_REGIONKEY,R_NAME,R_COMMENT','s3a://splice-benchmark-data/flat/HTAP/htap-1000/region.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://splice-benchmark-data/flat/HTAP/data-errors', true, null);
 
-call SYSCS_UTIL.IMPORT_DATA (current schema,'NATION','n_nationkey,n_name,n_regionkey,n_comment','s3a://htap-b/flat/HTAP/htap-1000/nation.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://htap-b/flat/HTAP/data-errors', true, null);
+call SYSCS_UTIL.IMPORT_DATA (current schema,'NATION','n_nationkey,n_name,n_regionkey,n_comment','s3a://splice-benchmark-data/flat/HTAP/htap-1000/nation.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://splice-benchmark-data/flat/HTAP/data-errors', true, null);
 
-call SYSCS_UTIL.IMPORT_DATA (current schema,'SUPPLIER','su_suppkey,su_name,su_address,su_nationkey,su_phone,su_acctbal,su_comment','s3a://htap-b/flat/HTAP/htap-1000/supplier.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://htap-b/flat/HTAP/data-errors', true, null);
+call SYSCS_UTIL.IMPORT_DATA (current schema,'SUPPLIER','su_suppkey,su_name,su_address,su_nationkey,su_phone,su_acctbal,su_comment','s3a://splice-benchmark-data/flat/HTAP/htap-1000/supplier.csv', null, '''', 'yyyy-MM-dd HH:mm:ss', null, null, 5, 's3a://splice-benchmark-data/flat/HTAP/data-errors', true, null);
 
 /* Part 4 - optimize database */ 
 
